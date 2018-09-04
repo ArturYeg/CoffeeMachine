8338d3bbe58d48e07de468ba7b68f823a3d10759
yeggart1988 <yeggart1988@gmail.com>
2018-09-05 00:57:01 +0500

    Добавьте файлы проекта.

---
+++ CoffeeMashine/CoffeeMashine.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeMashine
+{
+    class CoffeeMashine
+    {
+        private float coffee;
+        private float water;
+        private float sugar;
+
+        private int coins = 0;
+        Dictionary<int, int> coffeetypetoprice = new Dictionary<int, int>(10);
+        List<List<float>> coffeereceipt = new List<List<float>>(10);
+
+        public CoffeeMashine(string ingredientspath, string receiptspath)
+        {
+            using (StreamReader sr = new StreamReader(ingredientspath))
+            {
+
+                string ingredients = sr.ReadLine();
+                if (ingredients != null)
+                {
+                    string[] ingredientarray = ingredients.Split(' ');
+                    if (ingredientarray.Length == 3)
+                    {
+
+                        if (!float.TryParse(ingredientarray[0], out coffee))
+                        {
+                            //todo
+                        }
+
+                        if (!float.TryParse(ingredientarray[1], out water))
+                        {
+                            //todo
+                        }
+                        if (!float.TryParse(ingredientarray[2], out sugar))
+                        {
+                            //todo
+                        }
+                    }
+                }
+            }
+            using (StreamReader str = new StreamReader(receiptspath))
+            {
+                string receipt;
+                while ((receipt = str.ReadLine()) != null)
+                {
+                    string[] receiptarray = receipt.Split(' ');
+                    if (receiptarray != null)
+                    {
+                        if (receiptarray.Length == 4)
+                        {
+                            List<float> receiptfloat = receiptarray.Select(x => float.Parse(x)).ToList();
+                            coffeereceipt.Add(receiptfloat);
+                        }
+                    }
+                }
+            }
+        }
+        public void Check()
+        {
+            coins = 0;
+        }
+        public void ChooseCoffee(int type)
+        {
+
+            int price = (int)coffeereceipt[type][3];
+            if (coins >= price)
+            {
+                if (coffeereceipt[type][0] <= coffee || coffeereceipt[type][1] <= water || coffeereceipt[type][2] <= sugar)
+                {
+                    coins -= price;
+                    coffee -= coffeereceipt[type][0];
+                    water -= coffeereceipt[type][1];
+                    sugar -= coffeereceipt[type][2];
+
+                    Console.WriteLine("Coffee is Ready");
+                }
+
+                else
+                {
+                    Console.WriteLine("ne dostatochno ingridientov");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Your money is not enough.");
+            }
+        }
+        public void insertCoin(int coin)
+        {
+            if (coin == 50 || coin == 100 || coin == 200 || coin == 500)
+            {
+                coins += coin;
+            }
+            else
+            {
+                Console.WriteLine("Please enter the correct coins");
+            }
+        }
+        public int GetAvailableCoins()
+        {
+            return coins;
+        }
+    }
+}
---
+++ CoffeeMashine/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeMashine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CoffeeMashine cm;
+            try
+            {
+                cm = new CoffeeMashine(@"C:\Users\Artur\source\repos\CoffeeMashine\CoffeeMashine\Ingredients.txt",
+                    @"C:\Users\Artur\source\repos\CoffeeMashine\CoffeeMashine\Receipts.txt");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR: " + ex.Message);
+                Console.ReadLine();
+                return;
+            }
+            Console.WriteLine("Please choose the action");
+            Console.WriteLine();
+            Console.WriteLine("1. Enter the coin");
+            Console.WriteLine("2. Select Coffee");
+            Console.WriteLine("3. Pick up the coin");
+            Console.WriteLine("4. Exit");
+            Console.WriteLine();
+
+            while (true)
+            {
+                int number = Convert.ToInt32(Console.ReadLine());
+
+                if (number == 1)
+                {
+                    Console.WriteLine("Enter the coin");
+
+                    int coin = int.Parse(Console.ReadLine());
+                    cm.insertCoin(coin);
+                    Console.WriteLine("Balance =" + cm.GetAvailableCoins());
+                }
+                else if (number == 2)
+                {
+                    Console.WriteLine("Select Coffee");
+                    int coffeetype = Convert.ToInt32(Console.ReadLine());
+                    cm.ChooseCoffee(coffeetype);
+                    Console.WriteLine("Balance =" + cm.GetAvailableCoins());
+                }
+                else if (number == 3)
+                {
+                    Console.WriteLine("Pick up the coin");
+                    cm.Check();
+                    Console.WriteLine("Balance =" + cm.GetAvailableCoins());
+
+                }
+                else if (number == 4)
+                {
+                    Console.WriteLine("Exit");
+                    break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
